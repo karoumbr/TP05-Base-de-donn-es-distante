198bc7bbfb7da7b1bd51f7ba09dca050a81f4cfb
Karim Ben Romdhane <74336838+karoumbr@users.noreply.github.com>
2021-01-04 13:37:17 +0100

    Update insertion_produit.php

--- nomsiteweb/insertion_produit.php
+++ nomsiteweb/insertion_produit.php
@@ -1,12 +1,11 @@
 <?php
 $db = "bentechprotvdb";
-$ref = $_POST["ref"];
 $des = $_POST["des"];
 $host = "localhost";
 $conn = mysqli_connect($host, "root","",$db);
 if ($conn)
 {
-	$q= "insert into produits (id,designation) values ('$ref','$des')";
+	$q= "insert into produits (designation) values ('$des')";
 	if (mysqli_query($conn, $q)) {
 		echo "succes insertion";
 		
